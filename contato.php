30310527f47ef4d402e80d46d0d0ceeb10ee3399
Kelviny H <kelvinyhenrique17@gmail.com>
2020-05-03 10:42:44 -0300

    Funções

--- contato.php
+++ contato.php
@@ -35,6 +35,20 @@
                 }
             }
 
+            public function getInfo($id) {                
+                $sql = "SELECT * FROM contatos WHERE id = :id";
+                $sql = $this->pdo->prepare($sql);
+                $sql->bindValue(':id', $id);
+                $sql->execute();
+                
+                if($sql->rowCount() > 0) {
+                    return $sql->fetch();
+                } else {
+                    return array();
+                }
+
+            }   
+
         
         public function getAll() {
             $sql = "SELECT * FROM contatos";
@@ -47,17 +61,23 @@
             }
         }
 
-        public function editar($nome, $email) {
-            if($this->existeEmail($email) == true) {
-                $sql = "UPDATE contatos SET nome = :nome WHERE email = :email";
+        public function editar($nome, $email, $id) {
+            if($this->existeEmail($email) == false ) {
+           if($this->existeId($id) == true) {
+                $sql = "UPDATE contatos SET nome = :nome, email = :email WHERE id = :id";
                 $sql = $this->pdo->prepare($sql);
-                $sql->bindValue(":nome", $nome);
+                $sql->bindValue(":nome", $nome); 
                 $sql->bindValue(":email", $email);
+                $sql->bindValue(":id", $id);
                 $sql->execute();
                 return true;
             } else {
-                return false;
+                return 404;
             }
+            } else {
+                return true;
+            }
+ 
 
         }
 
@@ -72,6 +92,34 @@
                 return false;
             }
         }
+
+        
+        public function excluirId($id) {
+                if ($this->existeId($id) == true){
+                    $sql = "DELETE FROM contatos WHERE id = :id";
+                    $sql = $this->pdo->prepare($sql);
+                    $sql->bindValue(":id", $id);
+                    $sql->execute();
+                    return true;
+                } else {
+                    return false;
+                }
+        }
+
+
+        private function existeId($id) {
+            $sql = "SELECT * FROM contatos WHERE id = :id";
+            $sql = $this->pdo->prepare($sql);
+            $sql->bindValue(":id", $id);
+            $sql->execute();
+
+            if($sql->rowCount() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
 
         private function existeEmail($email) { 
             $sql = "SELECT * FROM contatos WHERE email = :email";
